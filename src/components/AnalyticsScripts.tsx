cd6e7bc57845b631c6e7a807f535ce25b6cbcc93
njofredev <nico.jofreandrade@gmail.com>
2026-06-24 14:51:23 -0400

    feat: add Microsoft Clarity tracking script

--- src/components/AnalyticsScripts.tsx
+++ src/components/AnalyticsScripts.tsx
@@ -92,7 +92,7 @@ export function AnalyticsScripts() {
             __html: `
               !function(f,b,e,v,n,t,s)
               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
               if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
               n.queue=[];t=b.createElement(e);t.async=!0;
               t.src=v;s=b.getElementsByTagName(e)[0];
@@ -103,6 +103,21 @@ export function AnalyticsScripts() {
           }}
         />
       )}
+
+      {/* 4. Microsoft Clarity */}
+      {<Script
+        id="clarity-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID || "r2hh1kkn17"}");
+          `,
+        }}
+      />}
     </>
   );
 }
