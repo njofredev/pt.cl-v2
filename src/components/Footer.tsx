890f4d4d52990403b9c4c739289fad8ddf061ffc
njofredev <nico.jofreandrade@gmail.com>
2026-06-30 12:46:38 -0400

    feat: add Privacy Policy link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -229,7 +229,8 @@ export const Footer = () => {
               { name: 'Inicio', href: '/' },
               { name: 'Quiénes Somos', href: '/nosotros' },
               { name: 'Nuestras Sucursales', href: '/#sucursales' },
-              { name: 'Estado de Sistemas', href: 'https://uptime.policlinicotabancura.cl/status/estado' }
+              { name: 'Estado de Sistemas', href: 'https://uptime.policlinicotabancura.cl/status/estado' },
+              { name: 'Política de Privacidad', href: 'https://policlinicotabancura.dentalink.cl/politicas-privacidad/wJMGyh8nYFC2bfENfYq1UD3c7lGG7K8' }
             ]}
           />
 
