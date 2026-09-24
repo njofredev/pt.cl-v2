91278294e9508b3902b6ded523a3ece44ac0cf5c
njofredev <nico.jofreandrade@gmail.com>
2026-09-24 15:40:16 -0300

    feat(professionals): agregar imagen y mapeo para perfil de Carolina Bombardiere

--- src/data/professionals.ts
+++ src/data/professionals.ts
@@ -227,6 +227,8 @@ export async function getProfessionals(): Promise<Professional[]> {
             image = "/img_profesionales/perfilCarmenGloriaMejias.jpg";
           } else if (lowerName.includes("carolina") && lowerName.includes("fones")) {
             image = "/img_profesionales/perfilCarolinaFones.png";
+          } else if (lowerName.includes("bombardiere")) {
+            image = "/img_profesionales/perfilCarolinaBombardiere.png";
           } else if (lowerName.includes("caterina") && lowerName.includes("benapres")) {
             image = "/img_profesionales/perfilCaterinaBenapres.jpg";
           } else if (lowerName.includes("consuelo") && lowerName.includes("olivares")) {
